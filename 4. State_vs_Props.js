dcac0bb490cf71a3d6a832541519c9f8b643d344
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-12 17:50:16 +0530

    Update 4. State_vs_Props.js

--- 4. State_vs_Props.js
+++ 4. State_vs_Props.js
@@ -1,1 +1,16 @@
 
+// State vs Props
+
+// state & props are related to each other.
+
+// State of a component will often becomes prop of child components.
+
+// props pass down to a child component from render method of a parent component.
+
+<MyChild name={this.state.childsName} />
+
+// in the example above 
+  
+// The parent's state value of childsName becomes the child's this.props.name
+  
+// normally props are immutable, if we need to change the value of props then its parent should update its state & then react will propagate / update its prop.
