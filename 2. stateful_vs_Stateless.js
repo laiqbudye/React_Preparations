b9707e9f357ead5be15e32be09b404bb56d4b9b8
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-12 16:32:02 +0530

    Update 2. stateful_vs_Stateless.js

--- 2. stateful_vs_Stateless.js
+++ 2. stateful_vs_Stateless.js
@@ -1,1 +1,13 @@
 
+// What do you mean by Stateful vs Stateless components.
+
+// the components which handles state inside them are called as Stateful components.
+
+// Historically class-based components were stateful components because in the previous version of react only class components are supposed to play with state.
+
+// but in react V16.8, hooks got introduced and using useState() we can now handle state in functional components also.
+
+// so coming back to our qustion, the components who are handling state inside them are referred as stateful components.
+
+// the components which does not manage state inside them are referred as stateless components / presemtational components / dumb components. they only view something on page
+//  based on data they get
