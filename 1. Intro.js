3db55eac437200b6eca14e027b8614c5d1e82d6c
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-12 16:09:30 +0530

    Update 1. Intro.js

--- 1. Intro.js
+++ 1. Intro.js
@@ -1,1 +1,12 @@
 // React is a javascript library for building user interfaces.
+
+import React from 'react';
+
+const title = React.createElement('h1', {}, 'My First React Code');
+
+/*
+here,
+  h1 - is the component (type of element we are creating),
+    {} - is the props (is an object containig properties),
+      My First React Code - children
+*/
