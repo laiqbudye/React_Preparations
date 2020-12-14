54ac3fee44f2a3690a2b58e172c25f802aefd84c
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-15 01:25:51 +0530

    Create 10. HOC.js

---
+++ 10. HOC.js
@@ -0,0 +1,19 @@
+Higher order components
+a higher-order component is a function/component that takes a another component as i/p and returns a new component.
+
+
+for e.g. AUX component ( that i made in zenconnect )
+
+const aux = props => props.children
+
+export default aux;
+
+
+and in a component we can write in render method like
+
+render(){
+  <Aux>   // it will behave like React.Fragment() in this case
+    <component 1 />
+    <component 2 />
+  </Aux>
+}
