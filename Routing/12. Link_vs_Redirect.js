b0d9f452a2a3fe9a29ccd8dfbf4f29f38c693c24
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-20 04:12:04 +0530

    Create 12. Link_vs_Redirect.js

---
+++ Routing/12. Link_vs_Redirect.js
@@ -0,0 +1,9 @@
+LINK
+when we link page, then it changes URL in the address bar. 
+it pushes new URL in the history API, so if we click back button then we can go to the previous page.
+
+
+Redirect
+this also changes URL in address bar.
+The main difference is that redirect replaces current page with new URL, so if we press back then we cant go to the previous page as it does not exist in stack.
+instead on back press we go the page we visit before that page.
