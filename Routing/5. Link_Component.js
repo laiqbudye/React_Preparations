ed83eec2c57d2c869e51cc0d2bdb3e75929bf08f
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-20 01:26:21 +0530

    Create 5. Link_Component.js

---
+++ Routing/5. Link_Component.js
@@ -0,0 +1,28 @@
+Link component is also part of react-router-dom.
+
+in the normal application if we want to go to another page then we write <a href='google.com'>Go to goole</a>.
+
+this thing is allowed in react also, but the problem is that whenever we click on anchor tags it sends new request to the server & our page gets reload, so in this case our entire
+app state gets refreshed.
+
+so to avoid such problems Link component from react-router is very useful.
+
+<Link to='google.com'>Go to goole</Link>
+
+here it wont send new request & our page does not get reload. underneath the hood react considers it as anchor tag & it block the new request with preventDefault().
+to prop defines the location to navigate to.
+
+example:-
+export const Home = () => (
+  <div>
+    Home Component
+    <ul>
+      <li>
+        <Link to=”/items”>Items</Link>
+      </li>
+      <li>
+        <Link to=”/category”>Category</Link>
+       </li>
+    </ul>
+  </div>
+);
