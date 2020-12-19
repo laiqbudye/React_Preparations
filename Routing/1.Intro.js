463e6161ba06ea438b0675c6374615fb996d8f34
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-19 20:40:43 +0530

    Create 1.Intro.js

---
+++ Routing/1.Intro.js
@@ -0,0 +1,14 @@
+What is Routing?
+Routing is the ability to move between different parts of an application when a user enters a URL or clicks an element (link, button, icon, image etc) within the application.
+
+Packages we need to use:- 
+=> react-router: the core library.
+=> react-router-dom: a variant of the core library meant to be used for web applications.
+=> react-router-native: a variant of the core library used with react native in the development of Android and iOS applications.
+
+Often, there is no need to install the core react-router library by itself, but rather a choice is made between react-router-dom and react-router-native, depending on the situation. 
+Both react-router-dom and react-router-native import all the functionality of the core react-router library.
+
+These packages are not created by Facebook but these pkgs are standard to use in react for routing.
+
+to install this pkg:- npm install --save react-router-dom
