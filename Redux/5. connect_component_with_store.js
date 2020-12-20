3bcaacb0ddd079e12a00532dd03e588b9b6997fd
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-21 02:21:57 +0530

    Update 5. connect_component_with_store.js

--- Redux/5. connect_component_with_store.js
+++ Redux/5. connect_component_with_store.js
@@ -17,7 +17,7 @@ for connect() method we need to pass mapStateToProps, mapDispatchToProps as a pa
 mapStateToProps:
 this function expects redux state as a input & then maps our components props to the state from store.
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state) => {   // here we get access to state object using connect method
   return {
     ctr: state.counter ;  // ctr is a local prop which is mapping to counter state
   }
@@ -25,3 +25,39 @@ const mapStateToProps = (state) => {
 
 so in this case we can write our connect method as 
 connect(mapStateToProps)(Login)
+
+
+
+
+mapDispatchToProps:
+const mapDispatchToProps = (dispatch) => {   // here we get access to dispatch method using connect method..... this is equivalent to store.dispatch that we saw in Setup_store.js
+  return {
+    onIncrementCounter: () => dispatch({type: 'INCREMENT'});
+  }
+}
+
+
+this method can be used to dispatch actions from our component. while setting up store with node.js we dispatched an action using store.dispatch(), but in our react
+component we dont have direct access to store object.
+
+we get that access using connect method.
+
+connect(mapStateToProps, mapDispatchToProps)(Login);
+
+if we dont want to pass / dont have mapStateToProps then we need to pass it as null & can write like,
+  
+connect(null, mapDispatchToProps)(Login);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
