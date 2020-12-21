bf7f4f48c3d9e9d7dc3a7e9b2b3e78268e496e5f
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-21 19:30:47 +0530

    Create 7. Middlewares.js

---
+++ Redux/7. Middlewares.js
@@ -0,0 +1,14 @@
+middleware is a function that gets execute between action gets dispatched & reach to reducer.
+basically middleware can be used to logging, crash reporting, talking to async API.
+
+redux on its own is synchronous, then how it handles its async tasks like network requests, here middleware comes in picture.
+
+applyMiddleware() is a function from redux that allows us to add middleware to our store. 
+
+e.g
+
+const store  createStore(rootReducer, applyMiddleware(middlewarename));
+
+
+we can also use redux devtools chrome extention & use it as middleware for debugging purpose.
+
