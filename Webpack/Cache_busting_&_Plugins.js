802701e1bed37bb317af96db3dd0ef147678b9a3
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-01-14 03:16:54 +0530

    Update Cache_busting_&_Plugins.js

--- Webpack/Cache_busting_&_Plugins.js
+++ Webpack/Cache_busting_&_Plugins.js
@@ -21,3 +21,31 @@ module.exports = {
 so till this step we have created main.js with dynamic hashcode which will change every time when something changes in code.
 
 so how do we include this dynamic main.js in our html file, so here we require plugins
+
+
+-------------------------------------------------------------------------------------------------------------------------------
+  
+  what is plugin?
+    it is a option used to customize webpack build process in a variety of ways.
+  we need to pass plugins as an array.
+  
+  HtmlWebpackPlugin: this is especially useful for webpack bundles that contains hashcode in the file name which changes on every compilation.
+  this plugin generates html file (index.html) for us including <script src= main.hash.js>. we can supply our own template also.
+  
+  module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contentHash].js",  // applying hash... // from webpack V5 contentHash replaced with contenthash (no capital H)
+    path: path.resolve(__dirname, "dist");  // dist is a folder name... src/dist/main.5asa35345jkhjwjbf\shj.js
+  },
+  
+  plugins: [new HtmlWebpackPlugin()]   // it will create index.html inside dist folder with scipt src set to main.hash.js
+}
+  
+//to supply our own template we need to pass this as follows:- 
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html'   // it will take our template.html file & make index.html file with same code & add <script src= main.hash.js> inside of that file.
+  })]
+  
+  
+  
