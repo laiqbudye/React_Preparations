57c4462b53b94960d39850b8899ba02b2479bc40
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-11-18 15:07:08 +0530

    Create 1.Intro.js

--- Redux/reselect/1.Intro.js
+++ Redux/reselect/1.Intro.js
@@ -1,7 +1,8 @@
 reselect:-
   - is a library for creating memoized "selector" functions.
   - commonly used with redux but can be used with plane JS as well
-  
+  - this library returns createSelector function 
+
   to install:- npm install reselect  (we need to install it externally in traditional redux)
   
   it is now officially added to redux toolkit pkg,
@@ -26,7 +27,7 @@ selector:-
     
     
    in the above code we are mapping redux state with local props (users: state.users.users). if we have 100 components then we need to write same code again & again.
-   to avoid this we can wite one single selector function & use that in all 100 components.
+   to avoid this we can write one single selector function & use that in all 100 components.
    
    e.g
    selectors.js
@@ -42,3 +43,74 @@ selector:-
     }
   }
    
+ 
+Why Reselect:-
+  to understand this lets first understand problem with normal approach.
+  
+  lets say we need to filter a list & return only elements based on search text
+  
+  e.g
+  
+  selectors.js
+
+  export const usersSelector = (state) => state.users.users;
+
+  export const filteredUsersSelector = (state) => {
+      usersSelector(state).filter(user => {
+          console.log("filtering users");
+          return user.includes(state.users.search);
+      })
+  }
+
+   
+  app.js
+  const mapStateToProps = (state) => {
+  console.log(state)
+
+  return {
+    users: usersSelector(state),  
+    username: state.users.username,
+    search: state.users.search, 
+    filteredUsers: filteredUsersSelector(state)    // here we are calling function from selectors.js
+  }
+}
+  
+  the problem with above approach is, whenever state updates in our app in any component, mapStateToProps gets called which then executes filteredUsers function.
+  so if we are filtering a large list then to execute on every state update, it will impact on performance.
+  
+  
+  so to address this issue "RESELECT" comes into picture.
+  as definition says, it is a library for creating memoized "selector" functions.
+  
+  
+  1. this library returns "createSelector" function. 
+  2. every functions except last fucntions provided to createserver are dependencies.
+  3. Reselect implements memoization pattern
+  
+  same e.g with reselect
+  
+  import { createSelector } from "reselect";
+
+  export const filteredUsersSelector = createSelector(
+    state => state.users.users,         // here users & search are deps for this func i.e when any one of them changes then it will re execute the func
+    state => state.users.search,
+    (users, search) => {                // here users & search coming from line 95 & 96... this sequence shoud me maintained
+        return users.filter(user => {
+            console.log("filtering users");
+            return user.includes(search);
+        })
+    }
+)
+  
+  
+  app.js
+  const mapStateToProps = (state) => {
+    return {
+      users: usersSelector(state),  
+      username: state.users.username,
+      search: state.users.search,
+      filteredUsers: filteredUsersSelector(state)    // here we are calling reselect's func
+    }
+  }
+
+So with the above approach, when users or search i.e. deps passed to func changes, then only filteredUsers func will get execute. (using memoization technique)
